cdbee3a4e67ac85e1ac6ba50d89b4e9f935ca6d9
theMkrv <themkrv@gmail.com>
2019-11-05 10:20:53 +0700

    Хай

--- Project3.py
+++ Project3.py
@@ -3,17 +3,19 @@ step = 0
 letter = 0
 start = input(" Хотите измерить длину слова? \n").lower()
 
-if start == "да":
-    while start == "да":
+if start == "да" or start == "yes":
+    while start == "да" or start == "yes":
         word = input("\n Введите слово: ")
-        print("В", check, "слове", len(word), "букв.")
+        print("В", check, "слове", len(word), "символов.")
         check += 1
         start = input("Хотите измерить длину слова? \n").lower()
         step += 1
         letter += len(word)
 
-    print("\n Вы посчитали количество букв в", step, "словах. \n" + "И написали в общей сумме", letter, "букв.")
-
+    if letter == 0:
+        print("\n Символов не было введенно.")
+    else:
+        print("\n Вы посчитали количество символов в", step, "словe(ах). \n" + "И написали в общей сумме", letter, "символов.")
 else:
     print("\n Очень жаль! \n" + "До свидания!")
 
